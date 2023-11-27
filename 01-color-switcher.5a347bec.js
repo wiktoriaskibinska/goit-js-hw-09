95266371227b246f95f58c63a8977ed9f43623bb
wiktoriaskibinska <119757706+wiktoriaskibinska@users.noreply.github.com>
2023-11-27 12:27:28 +0000

    Deploying to gh-pages from @ wiktoriaskibinska/goit-js-hw-09@29aefe321b95fe575298263ae298cb4b21063819 🚀

---
+++ 01-color-switcher.5a347bec.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=document.querySelector("body"),o=null;t.addEventListener("click",(function(){t.disabled=!0,e.disabled=!1,o=setInterval((function(){n.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1500)})),e.addEventListener("click",(function(){t.disabled=!1,e.disabled=!0,clearInterval(o)}))}();
+//# sourceMappingURL=01-color-switcher.5a347bec.js.map
